2999b711957cf83d703032234fdf9140e6dbf327
MiLiahkim00000101 <mihas05@mail.ru>
2025-02-20 19:19:20 +0700

    new accuracy of logic in files main.cpp

--- Task1/with_Cmake/main.cpp
+++ Task1/with_Cmake/main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <fstream>
 
 #ifdef FLOATVALUESIN
     float temp = 0;
-    float temp_sin_val;
-    std::vector<float> sin_values; 
+    float temp_sin_val; 
     float res = 0;
 #else
     double temp = 0;
     double temp_sin_val;
-    std::vector<double> sin_values;
     double res = 0;
 #endif
 
-size_t size_of_temp = sizeof(temp);
 
 int main(){
+
     
     for (int i = 0; i < 10000000; ++i){
 
         temp_sin_val = sin(temp);
-
         res += temp_sin_val;
-        sin_values.push_back(temp_sin_val);
-        temp += 2 * M_PI / 10000000; // здесь за такое огромное количество итераций если выбран
+        temp = (2 * M_PI * i) / 10000000; // В этом случае мы довольно точно обозначаем число
+        // temp и не аккумулируем его, как следствие счет получается довольно точен как для double так и для float
+        
+        
+        // Неверный способ использования: temp += 2 * M_PI / 10000000;
+        // здесь за такое огромное количество итераций если выбран
         // float накапливается большая ошибка из игнорирования малых значений после запятой
         //(мне кажется, что ошибка накапливается за счет игнорирования значений
         // после 10 знака(после запятой) до того, как мы получили целую часть и после 5-7 после). 
@@ -32,9 +34,9 @@ int main(){
         // тем менее точно оно будет определяться.
         // Здесь вышла именно такая ситуация, ведь отдаляясь от нуля значение temp начинает меняться
         // все медленнее и медленнее, в итоге мы даже не можем пройти весь период((((
-        //std::cout << i << ", val: " << temp_sin_val << std::endl;
 
-        // В случае с float здесь точность до 3 знаков после запятой ну то бишь почти ноль
+
+        
     }
 
     std::cout << res << std::endl;
